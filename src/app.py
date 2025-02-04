92bd5a6e8ef118b9d8abb1431ca3810bb8dabcd9
Dudi <69299652+Daniel-Dudek@users.noreply.github.com>
2025-02-04 15:14:37 +0000

    Star Wars Rest API project done

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import Favourites, FavouritesType, People, Planets, Species, db, Users
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,107 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+tables = {
+    "species": Species,
+    "planets": Planets,
+    "people": People
+}
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/users', methods=['GET'])
+def get_users():
+    user_list = Users.query.all()
+    return jsonify(user_list), 200
 
-    return jsonify(response_body), 200
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = Users.query.get(user_id)
+    return jsonify(user) if user else (jsonify({"error": "User not found"}), 400)
+
+@app.route('/users/<int:user_id>/favourites', methods=['GET', 'POST', 'DELETE'])
+def handle_favourites(user_id):
+    if request.method == 'GET':
+        favourites = Favourites.query.filter_by(user_id=user_id).all()
+        return jsonify(favourites), 200
+    
+    data = request.get_json()
+
+    if request.method == 'POST':
+        required_fields = ["external_id", "type", "name"]
+        if not all(field in data for field in required_fields):
+            return jsonify({"error": "Missing required fields"}), 400
+        if not (data["type"] in FavouritesType.__members__):
+            return jsonify({"error": "Type no valid"}), 400
+        if Favourites.query.filter_by(external_id=data["external_id"], user_id=user_id, type=data["type"]).first():
+            return jsonify({"error": "Resource already favourited"}), 400
+        if not tables[data["type"].lower()].query.filter_by(id=data["external_id"]).first():
+            return jsonify({"error": "Resource not found"}), 400
+
+        new_favourite = Favourites(
+            user_id=user_id,
+            external_id=data["external_id"],
+            type=data["type"],
+            name=data["name"]
+        )
+
+        db.session.add(new_favourite)
+        db.session.commit()
+
+        return jsonify({
+            "message": "Favorite added successfully",
+            "favorite": new_favourite
+        }), 201
+    
+    if request.method == 'DELETE':
+        required_fields = ["id"]
+        if not all(field in data for field in required_fields):
+            return jsonify({"error": "Missing required fields"}), 400
+
+        favourite = Favourites.query.filter_by(id=data["id"], user_id=user_id).first()
+
+        if not favourite:
+            return jsonify({"error": "Favourite not found"}), 404
+
+        db.session.delete(favourite)
+        db.session.commit()
+
+        return jsonify({"message": "Favourite deleted successfully"}), 200
+
+@app.route('/species', methods=['GET'])
+def get_species():
+    species = Species.query.all()
+    return jsonify(species), 200
+
+@app.route('/species/<int:specie_id>', methods=['GET'])
+def get_specie(specie_id):
+    specie = Species.query.get(specie_id)
+    return jsonify(specie) if specie else (jsonify({"error": "Specie not found"}), 400)
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    return jsonify(planets), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Species.query.get(planet_id)
+    return jsonify(planet) if planet else (jsonify({"error": "Planet not found"}), 400)
+
+@app.route('/people', methods=['GET'])
+def get_characters():
+    characters = People.query.all()
+    return jsonify(characters), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_character(people_id):
+    result = (
+        db.session.query(People, Planets)
+        .join(Planets, People.homeworld == Planets.uid)
+        .filter(People.uid == people_id)
+        .first()
+    )
+    character, home_world = result
+    character.home_world = home_world
+    return jsonify(character) if character else (jsonify({"error": "Character not found"}), 404)
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,74 @@
+from dataclasses import dataclass , field
+from sqlalchemy import ForeignKey
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+@dataclass
+class Users(db.Model):
+    __tablename__ = 'users'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id:int = db.Column(db.Integer, primary_key=True, index=True, nullable=False)
+    name:str = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<Users(id={self.id}, name={self.name})>'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class FavouritesType(str, enum.Enum):
+    SPECIES = "SPECIES"
+    PLANETS = "PLANETS"
+    PEOPLE = "PEOPLE"
+
+@dataclass
+class Favourites(db.Model):
+    __tablename__ = 'favourites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id:int = db.Column(db.Integer, primary_key=True, index=True, nullable=False)
+    user_id:int = db.Column(db.Integer, ForeignKey('users.id'),index=True, nullable=False)
+    external_id:int = db.Column(db.Integer, nullable=False)
+    type:FavouritesType = db.Column(db.Enum(FavouritesType), nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f'<Favorites(id={self.id}, user_id={self.user_id}, name={self.name}, type={self.type})>'
+
+@dataclass
+class Species(db.Model):
+    __tablename__ = 'species'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id:int = db.Column(db.Integer, primary_key=True, unique=True, index=True, nullable=False)
+    description:str = db.Column(db.String(250), nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
+    homeworld:int = db.Column(db.Integer, ForeignKey('planets.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Species(uid={self.id}, name={self.name}, homeworld={self.homeworld})>'
+
+@dataclass
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id:int = db.Column(db.Integer, primary_key=True, unique=True, index=True, nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
+    gravity:str = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Planets(uid={self.id}, name={self.name}, gravity={self.gravity})>'
+
+@dataclass
+class People(db.Model):
+    __tablename__ = 'people'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id:int = db.Column(db.Integer, primary_key=True, unique=True, index=True, nullable=False)
+    description:str = db.Column(db.String(50), nullable=False)
+    name:str = db.Column(db.String(30), nullable=False)
+    homeworld:int = db.Column(db.Integer, ForeignKey('planets.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<People(uid={self.id}, name={self.name}, homeworld={self.homeworld})>'
